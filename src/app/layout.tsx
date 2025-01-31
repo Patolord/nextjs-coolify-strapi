a854b5be69dba8bedd00871bf9eff6a196999bbf
Patolord <rodrigo_luizp@hotmail.com>
2025-01-31 18:04:55 -0300

    Refactor font imports and remove unused fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,8 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 
 import Footer from "@/app/components/footer";
 import Header from "@/app/components/header";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -28,9 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <div className="bg-gray-200 min-h-screen grid grid-rows-[auto_1fr_auto]">
           <Header />
           <main className="container mx-auto bg-white/50 rounded-xl py-7 px-8 m-6 overflow-hidden">
